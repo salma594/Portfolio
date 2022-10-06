e3d35218fe3353567e4314db1c3b4e55f423e5dd
salma <saidisalma594@gmail.com>
2022-10-06 18:03:18 +0100

    h

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Navbar from "./components/nav/navbar"
+import Intro from "./components/intro/introd"
+import Service from "./components/Services/Service"
+import Experience from "./components/Experiences/Experience"
+import Portfolio from "./components/Portfolios/Portfolio"
+import './App.css'
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Navbar/>
+       <Intro/>
+       <Service/>
+       <Experience/>
+       <Portfolio/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
